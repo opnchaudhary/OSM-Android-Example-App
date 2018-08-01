c4874a44292688a0dd2b9b218ac2f4c32265277a
Paras Nath Chaudhary <opnchaudhary@gmail.com>
2018-08-01 22:35:09 +0545

    added marker

--- app/src/main/java/com/tuxkiddos/apps/locationtracker/MainActivity.kt
+++ app/src/main/java/com/tuxkiddos/apps/locationtracker/MainActivity.kt
@@ -8,16 +8,23 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.support.v7.content.res.AppCompatResources
 import org.osmdroid.api.IMapController
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var map: MapView
     private lateinit var context: Context
     private lateinit var mapController: IMapController
+    private lateinit var mRotationGestureOverlay: RotationGestureOverlay
+    private lateinit var mLocationOverlay: MyLocationNewOverlay
 
     private val permissions = arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -40,15 +47,37 @@ class MainActivity : AppCompatActivity() {
 
         mapController = map.controller
 
+        // add zoom buttons and 2 fingure gesture
         map.setBuiltInZoomControls(true)
         map.setMultiTouchControls(true)
 
-        mapController.zoomIn(15)
-        mapController.setZoom(15)
-        mapController.setCenter(GeoPoint(27.700769,85.300140))
+        // move map on default view point
+        mapController.setZoom(16)
+        mapController.setCenter(GeoPoint(27.70362,85.31495))
 
+        // enable rotation gesture
+        mRotationGestureOverlay = RotationGestureOverlay(context, map)
+        mRotationGestureOverlay.isEnabled = true
+        map.overlays.add(mRotationGestureOverlay)
+
+        //add my location overlay
+        mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(context),map)
+        mLocationOverlay.enableMyLocation()
+        map.overlays.add(mLocationOverlay)
+
+        addMarker()
 
     }
+
+    private fun addMarker() {
+        val marker = Marker(map)
+        marker.position =GeoPoint(27.70771,85.31405)
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.title = "Durbar High School"
+        marker.icon = AppCompatResources.getDrawable(context,R.drawable.ic_marker)
+        map.overlays.add(marker)
+    }
+
     private fun arePermissionsEnabled(): Boolean{
         for(permission in permissions){
             if(ContextCompat.checkSelfPermission(this, permission)!=PackageManager.PERMISSION_GRANTED)
@@ -73,7 +102,7 @@ class MainActivity : AppCompatActivity() {
                     if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                         if (shouldShowRequestPermissionRationale(permissions[i])) {
                             AlertDialog.Builder(this)
-                                    .setMessage("Your error message here")
+                                    .setMessage("This permission is required to use the app.")
                                     .setPositiveButton("Allow") { _, _ -> requestMultiplePermissions() }
                                     .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
                                     .create()
@@ -84,8 +113,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
 
     override fun onResume() {
         super.onResume()
